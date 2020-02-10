1eb9a7400a830433d6c365ee9490d3ba3d7b1aa5
parthjd <parthjd28@gmail.com>
2020-02-10 17:26:13 +0000

    initial front-end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,9 +2,34 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  }).done(users => {
+    for (user of users) {
+      $("<div>")
+        .text(user.name)
+        .appendTo($("body"));
     }
-  });;
+  });
+});
+
+$(document).ready(function() {
+  $("#dish-item1-add").click(function(e) {
+    e.preventDefault();
+    let oldQty = parseInt($("#dish-item1-qty").val());
+    let newQty = oldQty + 1;
+    $("#dish-item1-qty").val(newQty);
+    $("#ordered-qty1").text(newQty);
+    $("#ordered-price1").text(newQty * 11.99);
+    $("#sub-total1").text($("#ordered-price1").val());
+  });
+
+  $("#dish-item1-minus").click(function(e) {
+    e.preventDefault();
+    let oldQty = parseInt($("#dish-item1-qty").val());
+    if (oldQty > 0) {
+      let newQty = oldQty - 1;
+      $("#dish-item1-qty").val(newQty);
+      $("#ordered-qty1").text(newQty);
+      $("#ordered-price1").text(newQty * 11.99);
+    }
+  });
 });
