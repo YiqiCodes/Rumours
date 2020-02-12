680356268dd9f9a214ad37406bbc7931269dacff
parthjd <parthjd28@gmail.com>
2020-02-12 22:09:54 +0000

    sub-total functionality added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,6 +12,7 @@ $(() => {
 });
 
 $(document).ready(function() {
+  let orderSubTotal = 0;
   $(".dish-add").click(function(e) {
     e.preventDefault();
     let id = $(this)
@@ -25,30 +26,32 @@ $(document).ready(function() {
     $(`#dish-item${id}-qty`).val(newQty);
     if ($(`#order-${id}`).length === 0) {
       $(".order-list").append(
-        ` <div id="order-${id}" class="order-detail ordered-detail">
-          <p id="ordered-item-${id}">${dishTitle}</p>
-          <p id="ordered-qty-${id}">${newQty}</p>
-          <p id="ordered-price-${id}">$${orderPrice}</p>
-        </div>`
+        ` <div id="order-${id}" class="ordered-detail">
+          <input class="ordered-item dish-name" id="ordered-item-${id}" name="${id}" value="${dishTitle}">
+          <input class="ordered-item" id="ordered-qty-${id}" name="quantity${id}"  value="${newQty}">
+          <input class="ordered-item price" name="price${id}" id="ordered-price-${id}" value="$${orderPrice}">
+          </div>`
       );
     } else {
-      $(`#ordered-qty-${id}`).html(newQty);
-      $(`#ordered-price-${id}`).html(`$${orderPrice}`);
+      $(`#ordered-qty-${id}`).val(newQty);
+      $(`#ordered-price-${id}`).val(`$${orderPrice}`);
     }
-    let orderSubTotal = 0;
 
-    for (let i = 0; i <= $(`#order-${id}`).length - 1; i++) {
-      orderSubTotal += Number(orderPrice);
-    }
-    console.log(orderSubTotal);
-    $("#sub-total-price").html(`$${orderSubTotal}`);
-    // let orderTotal = Number($("#sub-total-price").html());
-    // let orderTotalTax = (orderTotal * 1.13).toFixed(2);
-    // $("#total-price").html(orderTotalTax);
+    orderSubTotal += Number(itemPrice);
+
+    let subTotal = orderSubTotal.toFixed(2);
+    $("#sub-total-price").html(`$${subTotal}`);
+
+    let orderTotal = subTotal;
+    let taxTotal = (orderTotal * 0.13).toFixed(2);
+    $("#tax-price").html(`$${taxTotal}`);
+    let orderTotalTax = (orderTotal * 1.13).toFixed(2);
+    $("#total-price").html(`$${orderTotalTax}`);
   });
 
   $(".dish-minus").click(function(e) {
     e.preventDefault();
+
     let id = $(this)
       .closest(".dish-item")
       .attr("id");
@@ -62,24 +65,27 @@ $(document).ready(function() {
       if ($(`#order-${id}`).length === 0) {
         $(".order-list").append(
           ` <div id="order-${id}" class="order-detail ordered-detail">
-          <p id="ordered-item-${id}">${dishTitle}</p>
-          <p id="ordered-qty-${id}">${newQty}</p>
-          <p id="ordered-price-${id}">$${orderPrice}</p>
+          <input id="ordered-item-${id}" name="${id}" value="${dishTitle}">
+          <input id="ordered-qty-${id}" name="quantity${id}"  value="${newQty}">
+          <input class="ordered-price" name="price${id}" id="ordered-price-${id}" value="$${orderPrice}">
           </div>`
         );
       } else if (newQty === 0) {
         console.log("hello");
         $(`#order-${id}`).remove();
       } else {
-        $(`#ordered-qty-${id}`).html(newQty);
-        $(`#ordered-price-${id}`).html(`$${orderPrice}`);
+        $(`#ordered-qty-${id}`).val(newQty);
+        $(`#ordered-price-${id}`).val(`$${orderPrice}`);
       }
+      orderSubTotal -= Number(itemPrice);
+
+      let subTotal = orderSubTotal.toFixed(2);
+      $("#sub-total-price").html(`$${subTotal}`);
+      let orderTotal = subTotal;
+      let taxTotal = (orderTotal * 0.13).toFixed(2);
+      $("#tax-price").html(`$${taxTotal}`);
+      let orderTotalTax = (orderTotal * 1.13).toFixed(2);
+      $("#total-price").html(`$${orderTotalTax}`);
     }
-    let orderSubTotal = Number($(`#ordered-price${id}`).html());
-    console.log(orderSubTotal);
-    $("#sub-total-price").html(orderSubTotal);
-    let orderTotal = Number($("#sub-total-price").html());
-    let orderTotalTax = (orderTotal * 1.13).toFixed(2);
-    $("#total-price").html(orderTotalTax);
   });
 });
